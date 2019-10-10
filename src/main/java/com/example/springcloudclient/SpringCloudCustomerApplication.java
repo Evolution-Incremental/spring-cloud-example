487f247b62ac6c6f8793ccb5494d352e94985f96
shaoshuai <shaoshuai@lumlord.com>
2019-10-10 18:41:56 +0800

    服务部署

--- src/main/java/com/example/springcloudclient/SpringCloudCustomerApplication.java
+++ src/main/java/com/example/springcloudclient/SpringCloudCustomerApplication.java
@@ -3,7 +3,6 @@ package com.example.springcloudclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
@@ -13,6 +12,8 @@ public class SpringCloudCustomerApplication {
 
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(SpringCloudCustomerApplication.class, args);
     }
 
--- src/main/java/com/example/springcloudclient/util/FeignClientUtil.java
+++ src/main/java/com/example/springcloudclient/util/FeignClientUtil.java
@@ -1,11 +1,6 @@
 package com.example.springcloudclient.util;
 
-import feign.Headers;
-import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
